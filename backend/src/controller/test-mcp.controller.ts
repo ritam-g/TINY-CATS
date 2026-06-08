bee744d9d9c7988d5d2546649520ce7b6a17f07c
ritam-g <ritammaty2006@gmail.com>
2026-06-08 18:43:21 +0530

    feat: update testMcpController to include cat breed recommendations and AI response

--- backend/src/controller/test-mcp.controller.ts
+++ backend/src/controller/test-mcp.controller.ts
@@ -1,19 +1,32 @@
 import type { Request, Response } from "express";
 import { testMcpService } from "../services/test-mcp.service.ts";
+import main from "../services/ai.service.ts";
 
 export async function testMcpController(req: Request, res: Response) {
-    const client =await testMcpService()
+    const client = await testMcpService()
 
-    const tools=await client.listTools()
+    const tools = await client.listTools()
 
-    const result=await client.callTool({
-        name:"getAllCats",
-        arguments:{}
+    const catData = await client.callTool({
+        name: "getCatBreedRecommendations",
+        arguments: {
+            apartmentFriendly: true,
+            kidsFriendly: true
+        }
     })
+    
+
+    const prompt = `
+    Avaialble Cats
+    ${JSON.stringify(catData.content[0].text)}
+    recommend me a cat breed that is good for apartment and good with kids
+    `
+
+    const aiResponse = await main(prompt)
 
     return res.status(200).json({
-        message:"MCP test successful",
+        message: "MCP test successful",
         tools,
-        result
+        result: aiResponse
     })
 }
